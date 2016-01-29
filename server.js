72af28177b1fd43b7d47eededab848294f6ce88a
vineetgarg1702 <vineetgarg2002@gmail.com>
2016-01-29 19:20:58 +0530

    with params

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
 var PORT =  process.env.PORT || 3000;
+
+var todos = [{
+	id: 1,
+	desc: 'meet mom for lunch',
+	completed: false
+},
+{
+	id:2,
+	desc: 'goto market',
+	completed: false
+},
+{
+	id:3,
+	desc: "do nodejs rest apis",
+	completed: false
+}];
+
+
+
 app.get('/', function(req,res)
 {
 	res.send('TODO api root');
@@ -9,4 +28,30 @@ app.get('/', function(req,res)
 app.listen(PORT,function()
 {
 	console.log("Listening at port numner: " + PORT);
+});
+
+//get request
+app.get('/todos',function(req,res)
+{
+	res.json(todos);
+});
+
+app.get('/todos/:id',function(req,res)
+{
+	var todoid;
+	 todoid = parseInt(req.params.id,10);
+	console.log(todoid + ' ' + todos.length);
+	for (var i = 0; i < todos.length; i++) {
+		if(todos[i].id === todoid)
+		{
+		 	
+		 	res.json(todos[i]);
+		 	break;
+		}
+		else
+		{
+			console.log('not found ' + i);
+		}
+	}
+	res.status(404).send();
 });
