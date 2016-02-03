0335bb2742bf95df193fba9f5e47b8e84c70f207
vineetgarg1702 <vineetgarg2002@gmail.com>
2016-02-03 13:02:08 +0530

    added query params

--- server.js
+++ server.js
@@ -39,7 +39,25 @@ app.listen(PORT,function()
 //get request
 app.get('/todos',function(req,res)
 {
-	res.json(todos);
+	//var query = req.query;
+	var filteredTodos = todos;
+	if(req.query.hasOwnProperty('completed'))
+	{
+		if(req.query.completed === 'true')
+		{
+			filteredTodos = _.where(filteredTodos, {completed:true});	
+		}
+		else if(req.query.completed === 'false')
+		{
+			filteredTodos = _.where(filteredTodos, {completed:false});
+		}
+		else
+		{
+			return res.json({});
+		}
+	}
+
+	res.json(filteredTodos);
 });
 
 app.get('/todos/:id',function(req,res)
@@ -56,6 +74,62 @@ app.get('/todos/:id',function(req,res)
 		res.status(404).send();
 	}
 });
+
+// delete todo item
+
+app.delete('/todos/:id',function(req,res){
+	var todoid = parseInt(req.params.id,10);
+	var matched = _.findWhere(todos,{id:todoid});
+	if(matched)
+	{
+		todos = _.without(todos,matched);
+		res.json({"success" : "Deleted...." + JSON.stringify(matched)});
+
+	}
+	else
+	{
+		res.status(404).json({"error" : "no data found with id " + todoid});
+	}
+
+})
+
+// update 
+
+app.put('/todos/:id', function(req,res)
+{
+	var todoid = parseInt(req.params.id);
+	var matched = _.findWhere(todos, {id : todoid});
+	if(!matched)
+	{
+		return res.status(404).json({"error": "requested id not found"});
+	}
+
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed))
+	{
+		validAttributes.completed = body.completed;
+	}
+	else if(body.hasOwnProperty('completed'))
+	{
+		return res.status(404).send();
+	}
+	
+	if(body.hasOwnProperty('description') && _.isString(body.description)  && body.description.trim().length > 0)
+	{
+		validAttributes.description = body.description;
+	}
+	else if(body.hasOwnProperty('description'))
+	{
+		return res.status(404).send();
+	}
+	
+	_.extend(matched,validAttributes);
+
+	res.json(matched);
+});
+
+
 
 // post request
 app.post('/todos', function(req,res)
